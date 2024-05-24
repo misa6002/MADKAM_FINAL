41b396c0cc17969ffe97a20edc99145253f44d80
misa6002 <150057243+misa6002@users.noreply.github.com>
2024-05-24 17:39:01 +0200

    Update copias_cameras.sh

--- copias_cameras.sh
+++ copias_cameras.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 source_dir="/usr/share/nginx/html/img"
-dest_dir_camera1="/media/madkam/copias/camera1_copias"
-dest_dir_camera2="/media/madkam/copias/camera2_copias"
-dest_dir_images="/media/madkam/copias/imagen_copias"
-dest_dir_grabacion1="/media/madkam/copias/grabacion1_copias"
-dest_dir_grabacion2="/media/madkam/copias/grabacion2_copias"
+dest_dir_camera1="/media/madkam/grab_img/camera1_copias"
+dest_dir_camera2="/media/madkam/grab_img/camera2_copias"
+dest_dir_images="/media/madkam/cp_total"
+dest_dir_grabacion1="/media/madkam/grab_img/grabacion1_copias"
+dest_dir_grabacion2="/media/madkam/grab_img/grabacion2_copias"
 
 rsync -av --ignore-existing "$source_dir/camera1"* "$dest_dir_camera1/"
 
@@ -16,3 +16,4 @@ rsync -av --ignore-existing "$source_dir/"* "$dest_dir_images/"
 rsync -av --ignore-existing "$source_dir/grabacion1"* "$dest_dir_grabacion1/"
 
 rsync -av --ignore-existing "$source_dir/grabacion2"* "$dest_dir_grabacion2/"
+
